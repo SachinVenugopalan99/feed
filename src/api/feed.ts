ccef67ef9cdcd7c4269cf9f82291d2fb5db9b45f
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2024-04-16 01:36:46 +0530

    version updation

--- src/api/feed.ts
+++ src/api/feed.ts
@@ -6,17 +6,17 @@ const API_KEY = 'XM0ooo4EG8puK9EPQ16M3KGxSA3ZsCKS';
 export default {
     async users() {
         return await apiInstance.get('/users', {
-            headers: {'Authorization': API_KEY}
+            headers: {'Authorization': API_KEY, "Access-Control-Allow-Origin": "*"}
         })
     },
     async feeds(params: any) {
         return await apiInstance.get(`/posts?${qs.stringify(params)}`, {
-            headers: {'Authorization': API_KEY}
+            headers: {'Authorization': API_KEY, "Access-Control-Allow-Origin": "*"}
         })
     },
     async feedById(id: any) {
         return await apiInstance.get(`/posts/${id}`, {
-            headers: {'Authorization': API_KEY}
+            headers: {'Authorization': API_KEY, "Access-Control-Allow-Origin": "*"}
         })
     }
 }
